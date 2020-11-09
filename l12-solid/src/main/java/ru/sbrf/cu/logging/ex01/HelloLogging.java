6c76137a1be1b0974a2923d2734dedab1841c851
kataus <kataus@gmail.com>
2020-11-09 17:11:37 +0300

    12 занятие. Примеры

---
+++ l12-solid/src/main/java/ru/sbrf/cu/logging/ex01/HelloLogging.java
@@ -0,0 +1,33 @@
+package ru.sbrf.cu.logging.ex01;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class HelloLogging {
+    private static Logger logger = LoggerFactory.getLogger(HelloLogging.class);
+
+    public static void main(String[] args) {
+        new HelloLogging().log();
+    }
+
+    private void log() {
+        String value = "test";
+
+//      Устаревший вариант
+//        if (logger.isDebugEnabled()) {
+//            logger.debug("Hello logging:" + value);
+//        }
+
+        //Современный вариант
+        logger.debug("Hello logging:{}", value);
+
+        try {
+            Thread.sleep(3_000);
+            throw new RuntimeException("exception for log");
+        } catch (Exception e) {
+            Thread.currentThread().interrupt();
+            logger.error("exception log:", e);
+        }
+    }
+}
