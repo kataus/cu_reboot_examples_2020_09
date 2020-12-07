6dd84662ecf41ac41ee0372dc54d14efec1d44d3
kataus <kataus@gmail.com>
2020-12-07 16:26:53 +0300

    19 занятие Spring

---
+++ l19-Spring/jdbc-demo-exercise/src/main/java/ru/sbrf/cu/spring/domain/Person.java
@@ -0,0 +1,28 @@
+package ru.sbrf.cu.spring.domain;
+
+public class Person {
+
+    private final long id;
+    private final String name;
+
+    public Person(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
